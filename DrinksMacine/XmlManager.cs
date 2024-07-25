09f0d2aef6df241a28fee22b49f28e19b863bcae
yhuda <strny3480@gmail.com>
2024-07-25 12:45:34 +0300

    we start build a class for manage the xml file, it already has a mathod to create or load a newgile

---
+++ DrinksMacine/XmlManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DrinksMacine
+{
+    internal class XmlManager
+    {
+
+        private string nameFile = "DarinksMechine.xml";
+        //opens or creates a xml file
+        public XmlDocument GetXmlDocument()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            if (File.Exists(nameFile))
+            {
+                try
+                {
+                    xmlDocument.Load(nameFile);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("error loading xml file: " + ex.Message);
+                }
+
+            }
+            else
+            {
+                XmlNode root = xmlDocument.CreateElement("Drinks");
+                xmlDocument.AppendChild(root);
+                xmlDocument.Save(nameFile);
+            }
+            return xmlDocument;
+
+        }
+
+    }
+}
